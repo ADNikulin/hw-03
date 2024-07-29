a0ecee2649b91b73e3bd6aef85a76660192b6beb
anikulin <sanches00790@gmail.com>
2024-07-30 02:34:28 +0300

    solution

--- locals.tf
+++ locals.tf
@@ -3,3 +3,21 @@ locals {
     user-data = "${file(var.path_to_meta_data_user)}"
   }
 }
+
+locals {
+  taks7 = {
+    "network_id" = "enp7i560tb28nageq0cc"
+    "subnet_ids" = [
+      "e9b0le401619ngf4h68n",
+      "e2lbar6u8b2ftd7f5hia",
+      "b0ca48coorjjq93u36pl",
+      "fl8ner8rjsio6rcpcf0h",
+    ]
+    "subnet_zones" = [
+      "ru-central1-a",
+      "ru-central1-b",
+      "ru-central1-c",
+      "ru-central1-d",
+    ]
+  }
+}
